95ffcd0652bc37b203ec83562038459b19877e6d
sharonfrancis91 <sharon.f@siriusemb.com>
2016-09-07 10:29:39 +0530

    fifth commit

--- src/test/java/businessLib/MyClass.java
+++ src/test/java/businessLib/MyClass.java
@@ -7,9 +7,14 @@ public class MyClass {
 
 	public void testclass()
 	{
+		Boolean flag=true;
 		 WebDriver driver = new FirefoxDriver();
 		  driver.get("https://in.linkedin.com/");
-
+		  Boolean present=false;
+		  if(flag.equals(present))
+		  {
+			  System.out.println("PASS");
+		  }
 	}
 
 }
